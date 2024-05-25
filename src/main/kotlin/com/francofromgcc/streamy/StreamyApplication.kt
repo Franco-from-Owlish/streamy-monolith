abcaec45756b4749c3bfe8d7300a36ff947ebe9b
Franco Grobler <franco@grobler.fyi>
2024-05-25 19:15:06 +0200

    Initial

---
+++ src/main/kotlin/com/francofromgcc/streamy/StreamyApplication.kt
@@ -0,0 +1,11 @@
+package com.francofromgcc.streamy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StreamyApplication
+
+fun main(args: Array<String>) {
+    runApplication<StreamyApplication>(*args)
+}
---
+++ src/test/kotlin/com/francofromgcc/streamy/StreamyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.francofromgcc.streamy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StreamyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
